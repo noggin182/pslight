5445dd8d689f6cf126deea2dfaf99161b1609dfe
Matthew Gregory <matthew@nogsnet.com>
2021-07-26 23:28:46 +0100

    fixed ws281x import

--- src/rpi/led-strip.ts
+++ src/rpi/led-strip.ts
@@ -4,7 +4,7 @@ import { LedManager } from '../led-manager';
 export async function attachWs281x(ledManager: LedManager): Promise<void> {
     if (isRpi()) {
         const ws281xModule = 'rpi-ws281x'; // keep the module name in a variable to prevent intellisense errors when the optional module is not installed
-        const ws281x = await import(ws281xModule);
+        const { default: ws281x } = await import(ws281xModule);
         ws281x.configure({
             leds: ledManager.length,
             stripType: 'grb'
